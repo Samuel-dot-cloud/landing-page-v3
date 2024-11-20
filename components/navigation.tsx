b78c0b781988b2103047d94ebd62015f8ebbdbd1
Samuel Wahome <samuelwahome.k5@gmail.com>
2024-11-20 23:39:52 +0300

    fix: stop sounds from playing on every page navigation or reload

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -11,7 +11,7 @@ import {
 } from "./navigation-elements";
 import Logo from "./logo";
 import {Howl} from 'howler';
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {useTheme} from "./use-theme";
 import {Tooltip} from "antd";
 
@@ -20,18 +20,47 @@ const Navigation = ({isGlobal}: { isGlobal: boolean }) => {
 
     const [click, setClick] = useState<boolean>(false);
 
-    const sound1 = new Howl({
-        src: ['/lights-on.wav']
-    });
-    const sound2 = new Howl({
-        src: ['/lights-off.mp3']
-    });
-    const sound3 = new Howl({
-        src: ['/volume-up.mp3']
-    });
-    const sound4 = new Howl({
-        src: ['/volume-down.mp3']
-    });
+    const sound1Ref = useRef(
+        new Howl({
+            src: ['/lights-on.wav'],
+        })
+    );
+
+    const sound2Ref = useRef(
+        new Howl({
+            src: ['/lights-off.mp3'],
+        })
+    );
+
+    const sound3Ref = useRef(
+        new Howl({
+            src: ['/volume-up.mp3'],
+        })
+    );
+
+    const sound4Ref = useRef(
+        new Howl({
+            src: ['/volume-down.mp3'],
+        })
+    );
+
+    const handleToggleMode = () => {
+        if (isSoundOn) {
+            sound3Ref.current.play();
+        } else {
+            sound4Ref.current.play();
+        }
+        toggleMode();
+    }
+
+    const handleToggleTheme = () => {
+        if (theme.type === "light") {
+            sound2Ref.current.play();
+        } else {
+            sound1Ref.current.play();
+        }
+        toggleTheme();
+    }
 
     const scrollTo = (id: string) => {
         let element = document.getElementById(id);
@@ -45,13 +74,6 @@ const Navigation = ({isGlobal}: { isGlobal: boolean }) => {
         setClick(!click);
     }
 
-    useEffect(() => {
-        theme.type === "light" ? sound2.play() : sound1.play();
-    }, [theme.type]);
-
-    useEffect(() => {
-        isSoundOn ? sound4.play() : null;
-    }, [isSoundOn]);
     return (
         <Section id="navigation">
             <NavBar>
@@ -69,12 +91,12 @@ const Navigation = ({isGlobal}: { isGlobal: boolean }) => {
                 </div> : null}
                 <div className="row">
                     <Tooltip title={theme.type === "light" ? "Switch to dark mode" : "Switch to light mode"}>
-                        <div className="desktop-1" onClick={toggleTheme} style={{cursor: "pointer"}}>
+                        <div className="desktop-1" onClick={handleToggleTheme} style={{cursor: "pointer"}}>
                             {theme.type === "light" ? <MoonIcon size={24}/> : <SunIcon size={24}/>}
                         </div>
                     </Tooltip>
                     <Tooltip title={isSoundOn ? "Mute sounds" : "Unmute sounds"}>
-                        <div className="desktop-2" onClick={toggleMode} style={{cursor: "pointer"}}>
+                        <div className="desktop-2" onClick={handleToggleMode} style={{cursor: "pointer"}}>
                             {isSoundOn ? <Volume2Icon size={24}/> : <VolumeXIcon size={24}/>}
                         </div>
                     </Tooltip>
