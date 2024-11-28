ffe3a37c80156c1a94d16143f0d6e705f8a18ecb
Samuel Wahome <samuelwahome.k5@gmail.com>
2024-11-28 20:53:06 +0300

    fix: make carousel image more responsive

--- components/carousel-elements.ts
+++ components/carousel-elements.ts
@@ -1,14 +1,14 @@
 import styled from "styled-components";
-
+import Image from "next/image";
 
 export const Container = styled.div`
   width: 25vw;
   height: 70vh;
-  
+
   @media (max-width: 70em) {
     height: 60vh;
   }
-  
+
   @media (max-width: 64em) {
     height: 50vh;
     width: 30vw;
@@ -23,16 +23,16 @@ export const Container = styled.div`
     height: 45vh;
     width: 60vw;
   }
-  
+
   .swiper {
     width: 100%;
     height: 100%;
   }
-  
+
   .swiper-slide {
-    background-color: ${props => props.theme.carouselColor};
+    background-color: ${(props) => props.theme.carouselColor};
     border-radius: 20px;
-    
+
     display: flex;
     justify-content: center;
     align-items: center;
@@ -46,4 +46,12 @@ export const Box = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+`;
+
+export const StyledImage = styled(Image)`
+  width: 100%;
+  height: auto;
+  border-radius: 10px;
+  object-fit: contain;
+  box-shadow: 0 4px 10px rgba(${(props) => props.theme.textRgba}, 0.2);
 `;
